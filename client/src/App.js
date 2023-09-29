e002d16b86115163f3ec3eeec7aef151231cf377
Kirk Thomas <dablumaroon@gmail.com>
2023-09-29 08:20:28 -0400

    Ready to create quote form

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,7 @@
 import "./App.css";
-
+import QuoteForm from "./components/freeQuoteForm";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <QuoteForm></QuoteForm>;
 }
 
 export default App;
---
+++ client/src/components/freeQuoteForm.js
@@ -0,0 +1,11 @@
+function freeQuoteForm() {
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default freeQuoteForm;
